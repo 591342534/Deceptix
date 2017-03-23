1eb47666fbee29befc56f28d133304cc8aa03f32
Sean Prashad <Smprashad@myseneca.ca>
2017-03-22 21:24:42 -0400

    Fixed spacing for comment block

--- RoboticControlSoftware/RoboticControlSoftware/Enumerations.h
+++ RoboticControlSoftware/RoboticControlSoftware/Enumerations.h
@@ -1,8 +1,8 @@
 /*
-Title:			BTN415 Project - Robotic Control Software - Winter 2016
-Author(s):		Sean Prashad, Hao Chen, Stephen Noble
-Student ID:		029-736-105, 022-905-152, 018-619-155
-Description:	~ Header file containing all enumerations used for the entire project
+	Title:			BTN415 Project - Robotic Control Software - Winter 2016
+	Author(s):		Sean Prashad, Hao Chen, Stephen Noble
+	Student ID:		029-736-105, 022-905-152, 018-619-155
+	Description:	~ Header file containing all enumerations used for the entire project
 */
 
 #ifndef PROJECT_PKTDEF_ENUMERATIONS_H
